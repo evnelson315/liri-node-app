7f4101280586a252c1c026a2bfc6330cb626e8d1
Evan Nelson <Evan Nelson.evnelson.315@gmail.com>
2016-06-28 18:58:06 -0500

    basic files added

---
+++ keys.js
@@ -0,0 +1,22 @@
+// 	Consumer Key (API Key)	q0I0UQwyLciOUdOE5TujPvOk4
+// Consumer Secret (API Secret)	GJVpp8coFWa4XBcBm2Xq8lKlYrQblf3uXs6DeMitkbK15ZLjnh
+// Access Level	Read and write (modify app permissions)
+// Owner	evnelson315
+// Owner ID	722593734324396032
+
+// 	Access Token	722593734324396032-HsCDzc3t4BOMHIclwLsbVyPhTjac2fe
+// Access Token Secret	oLCyOoECG3baoTBXkpcJiy5Qx46MU4l7tHnmor1N5GaOl
+// Access Level	Read and write
+// Owner	evnelson315
+// Owner ID	722593734324396032
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+
