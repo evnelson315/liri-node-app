ef9dffa1bab1a36af0df41ba407f6ddf8cb2637f
Evan Nelson <Evan Nelson.evnelson.315@gmail.com>
2016-07-01 20:57:48 -0500

    omdb complete

--- keys.js
+++ keys.js
@@ -13,10 +13,10 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '<q0I0UQwyLciOUdOE5TujPvOk4',
+  consumer_secret: 'GJVpp8coFWa4XBcBm2Xq8lKlYrQblf3uXs6DeMitkbK15ZLjnh',
+  access_token_key: '722593734324396032-HsCDzc3t4BOMHIclwLsbVyPhTjac2fe',
+  access_token_secret: 'oLCyOoECG3baoTBXkpcJiy5Qx46MU4l7tHnmor1N5GaOl',
 }
 
 
