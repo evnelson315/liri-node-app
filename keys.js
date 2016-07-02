bc95d8e355f90439dd7a9987437c360fa561be1f
Evan Nelson <Evan Nelson.evnelson.315@gmail.com>
2016-07-02 15:58:42 -0500

    no go

--- keys.js
+++ keys.js
@@ -1,22 +1,22 @@
-// 	Consumer Key (API Key)	q0I0UQwyLciOUdOE5TujPvOk4
-// Consumer Secret (API Secret)	GJVpp8coFWa4XBcBm2Xq8lKlYrQblf3uXs6DeMitkbK15ZLjnh
-// Access Level	Read and write (modify app permissions)
-// Owner	evnelson315
-// Owner ID	722593734324396032
-
-// 	Access Token	722593734324396032-HsCDzc3t4BOMHIclwLsbVyPhTjac2fe
-// Access Token Secret	oLCyOoECG3baoTBXkpcJiy5Qx46MU4l7tHnmor1N5GaOl
-// Access Level	Read and write
-// Owner	evnelson315
-// Owner ID	722593734324396032
-
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: '<q0I0UQwyLciOUdOE5TujPvOk4',
+var twit = require('twitteer'),
+	twitter = new twit ({
+		 consumer_key: '<q0I0UQwyLciOUdOE5TujPvOk4',
   consumer_secret: 'GJVpp8coFWa4XBcBm2Xq8lKlYrQblf3uXs6DeMitkbK15ZLjnh',
   access_token_key: '722593734324396032-HsCDzc3t4BOMHIclwLsbVyPhTjac2fe',
   access_token_secret: 'oLCyOoECG3baoTBXkpcJiy5Qx46MU4l7tHnmor1N5GaOl',
-}
+	});
 
+
+
+var count = 0,
+	util = require('util');
+
+twitter.stream('filter', {track:"love"}, function(stream){
+
+	stream.on('data', function(data){
+		console.log(util.inspect(data));
+		stream.destroy();
+		process.exit(0);
+	});
+});
 
--- liri.js
+++ liri.js
@@ -1,1 +1,26 @@
 var request = require(keys.js);
+
+
+// INSTRUCTIONS:
+
+
+// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+var request = require('request');
+
+// Store all of the arguments in an array 
+var nodeArgs = process.argv;
+
+
+var tweetName = "";
+
+var queryUrl = "";
+
+twitter.stream("filter", {track: "love"}, function(stream){
+
+	stream.on('data', function(data){
+
+	console.log(util.inspect(data));
+	
+	});
+});
+
